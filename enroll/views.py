90b62dc8a468d7532823d4d2e98b2ec3ce091ef3
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-01-11 01:38:28 +0600

    Create form and save data in database

--- enroll/views.py
+++ enroll/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from .forms import StudentRegistration
+from .models import User
 
 # Create your views here.
 # def home(request):
 #     return render(request, 'enroll/base.html')
 
 def add_show(request):
-    return render(request,'enroll/addandshow.html')
+    if request.method == "POST":
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            eml = fm.cleaned_data['email']
+            pswd = fm.cleaned_data['password']
+            reg = User(name=nm,email=eml,password=pswd)
+            reg.save()
+
+    else:
+        fm = StudentRegistration()
+    return render(request,'enroll/addandshow.html',{'form':fm})
